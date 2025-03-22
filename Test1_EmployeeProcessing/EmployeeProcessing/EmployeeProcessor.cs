a16b058fef43d9f19feab2cc9c5ceef572269580
LoftyTowers <pgunary@gmail.com>
2025-03-22 10:07:07 +0000

    Added method decorators

--- Test1_EmployeeProcessing/EmployeeProcessing/EmployeeProcessor.cs
+++ Test1_EmployeeProcessing/EmployeeProcessing/EmployeeProcessor.cs
@@ -2,6 +2,12 @@ namespace EmployeeProcessing
 {
     public class EmployeeProcessor
     {
+        /// <summary>
+        /// Takes in a list of Employees and returns a list of Active Employees.
+        /// </summary>
+        /// <param name="employees"></param>
+        /// <returns>A List of Active Employees</returns>
+        /// <exception cref="Exception"></exception>
         public List<Employee> ProcessEmployeeRecords(List<Employee> employees)
         {
             try
@@ -16,15 +22,21 @@ namespace EmployeeProcessing
             }
         }
 
+        /// <summary>
+        /// Takes in a list of emplyees and returns the average salary of all employees in the list.
+        /// </summary>
+        /// <param name="employees"></param>
+        /// <returns>double</returns>
+        /// <exception cref="Exception"></exception>
         public double CalculateAverageSalary(List<Employee> employees)
         {
             try
             {
-                return employees.Average(e => e.Salary);            
+                return employees.Average(e => e.Salary);
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"An error occurred while calculating the average employee salary: { ex.Message }");
+                Console.WriteLine($"An error occurred while calculating the average employee salary: {ex.Message}");
                 throw new Exception("An error occurred while calculating the average employee salary.", ex);
             }
         }
